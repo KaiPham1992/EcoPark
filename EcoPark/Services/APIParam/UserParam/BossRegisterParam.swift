46ea93e00371de1f318720a34e4a8eceb49a193d
Tu DV <tudv@Tus-MacBook-Pro.local>
2019-11-13 10:08:59 +0700

    Update param register boss

---
+++ EcoPark/Services/APIParam/UserParam/BossRegisterParam.swift
@@ -0,0 +1,114 @@
+//
+//  BossRegisterParam.swift
+//  EcoPark
+//
+//  Created by Tu DV on 11/13/19.
+//  Copyright © 2019 demo. All rights reserved.
+//
+
+import ObjectMapper
+
+class BossRegisterParam: BaseParam {
+    
+    var email: String?
+    var fullname: String?
+    var gender: String?
+    var birthday: String?
+    var identity_number: String?
+    var issued_by: String?
+    var issued_date: String?
+    var cmnd_img_before_src: String?
+    var cmnd_img_after_src: String?
+    var gpkd_img_before_src: String?
+    var gpkd_img_after_src: String?
+    var parking_name: String?
+    var parking_type_id: Int?
+    var number_place: Int?
+    var parking_address: String?
+    var time_start: String?
+    var time_end: String?
+    var code_tax: String?
+    var price: Int?
+    var package_price: Int?
+    var material: [String] = []
+    var parking_img_src: [String] = []
+    
+    override func mapping(map: Map) {
+        self.email <- map["email"]
+        self.fullname <- map["fullname"]
+        self.gender <- map["gender"]
+        self.birthday <- map["birthday"]
+        self.identity_number <- map["identity_number"]
+        self.issued_by <- map["issued_by"]
+        self.issued_date <- map["issued_date"]
+        self.cmnd_img_before_src <- map["cmnd_img_before_src"]
+        self.cmnd_img_after_src <- map["cmnd_img_after_src"]
+        self.gpkd_img_before_src <- map["gpkd_img_before_src"]
+        self.gpkd_img_after_src <- map["gpkd_img_after_src"]
+        self.parking_name <- map["parking_name"]
+        self.parking_type_id <- map["parking_type_id"]
+        self.number_place <- map["number_place"]
+        self.parking_address <- map["parking_address"]
+        self.time_start <- map["time_start"]
+        self.time_end <- map["time_end"]
+        self.code_tax <- map["code_tax"]
+        self.price <- map["price"]
+        self.package_price <- map["package_price"]
+        self.material <- map["material"]
+        self.parking_img_src <- map["parking_img_src"]
+    }
+    
+     init(
+        email: String?,
+        fullname: String?,
+        gender: String?,
+        birthday: String?,
+        identity_number: String?,
+        issued_by: String?,
+        issued_date: String?,
+        cmnd_img_before_src: String?,
+        cmnd_img_after_src: String?,
+        gpkd_img_before_src: String?,
+        gpkd_img_after_src: String?,
+        parking_name: String?,
+        parking_type_id: Int?,
+        number_place: Int?,
+        parking_address: String?,
+        time_start: String?,
+        time_end: String?,
+        code_tax: String?,
+        price: Int?,
+        package_price: Int?,
+        material: [String] = [],
+        parking_img_src: [String] = []) {
+        
+        super.init()
+        self.email = email
+        self.fullname = fullname
+        self.gender = gender
+        self.birthday = birthday
+        self.identity_number = identity_number
+        self.issued_by = issued_by
+        self.issued_date = issued_date
+        self.cmnd_img_before_src = cmnd_img_before_src
+        self.cmnd_img_after_src = cmnd_img_after_src
+        self.gpkd_img_before_src = gpkd_img_before_src
+        self.gpkd_img_after_src = gpkd_img_after_src
+        self.parking_name = parking_name
+        self.parking_type_id = parking_type_id
+        self.number_place = number_place
+        self.parking_address = parking_address
+        self.time_start = time_start
+        self.time_end = time_end
+        self.code_tax = code_tax
+        self.price = price
+        self.package_price = package_price
+        self.material = material
+        self.parking_img_src = parking_img_src
+    }
+    
+    required init?(map: Map) {
+        fatalError("init(map:) has not been implemented")
+    }
+    
+}
