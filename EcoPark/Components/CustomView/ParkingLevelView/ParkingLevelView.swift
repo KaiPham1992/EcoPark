979725ab17b16181d9bc5f60c55a3c4188119fe5
Henry <henry@vinova.com.sg>
2019-11-17 14:01:54 +0700

    Fix filter

--- EcoPark/Components/CustomView/ParkingLevelView/ParkingLevelView.swift
+++ EcoPark/Components/CustomView/ParkingLevelView/ParkingLevelView.swift
@@ -11,28 +11,58 @@ import UIKit
 class ParkingLevelView: BaseViewXib {
     
     @IBOutlet var listButtonStar: [UIButton]!
-    var star: [Int] = [1,2,3,4,5]
+    
+    // Default value if user not choose any thing
+    var star: [Int] = [1,2,3,4,5] {
+        didSet{
+            print(star)
+        }
+    }
+    
+    override func setUpViews() {
+        for i in 0...3 {
+            listButtonStar[i].isSelected = false
+        }
+    }
     
     @IBAction func btnFiveStarTapped() {
-        star = [1,2,3,4,5]
+        star = [5]
+        changeStatus(listButtonStar[0])
+        setUnSelected(listButtonStar[3])
     }
     
     @IBAction func btnFourStarTapped() {
-        star = [1,2,3,4]
+        star = [4]
+        changeStatus(listButtonStar[1])
+        setUnSelected(listButtonStar[3])
     }
     
     @IBAction func btnThreeStarTapped() {
-        star = [1,2,3]
+        star = [3]
+        changeStatus(listButtonStar[2])
+        setUnSelected(listButtonStar[3])
     }
     
-    @IBAction func btnStarTapped(_ sender: UIButton) {
-        for button in listButtonStar {
-            if button == sender {
-                button.backgroundColor = AppColor.color_0_129_255.withAlphaComponent(0.6)
-            } else {
-                button.backgroundColor = .white
-            }
+    @IBAction func btnAllStarTapped() {
+        star = [1,2,3,4,5]
+        changeStatus(listButtonStar[3])
+        setUnSelected(listButtonStar[0])
+        setUnSelected(listButtonStar[1])
+        setUnSelected(listButtonStar[2])
+    }
+    
+    private func changeStatus(_ button: UIButton) {
+        button.isSelected = !button.isSelected
+        if button.isSelected {
+            button.backgroundColor = AppColor.color_0_129_255.withAlphaComponent(0.6)
+        } else {
+            button.backgroundColor = .white
         }
+    }
+    
+    func setUnSelected(_ button: UIButton) {
+        button.isSelected = false
+        button.backgroundColor = .white
     }
 }
 
