151e7120490fd64d8021cd7093843958dfef8306
Tu DV <tudv@Tus-MacBook-Pro.local>
2019-12-26 13:00:29 +0700

    Fix loading wallet

--- EcoPark/Modules/MyWallet/Wallet/WalletViewController.swift
+++ EcoPark/Modules/MyWallet/Wallet/WalletViewController.swift
@@ -87,9 +87,9 @@ class WalletViewController: BaseViewController {
             self.fromToDatePicker.fieldFrom.text = date.toString(dateFormat: AppDateFormat.ddMMYYYYTransaction)
             self.presenter?.listWalletHistory.removeAll()
             if self.fromToDatePicker.fieldTo.text == "" {
-                self.presenter?.getWalletHistory(startDate: self.fromToDatePicker.fieldFrom.text!, toDate: Date().toString(dateFormat: AppDateFormat.ddMMYYYYTransaction), showLoading: true)
+                self.presenter?.getWalletHistory(startDate: self.fromToDatePicker.fieldFrom.text!, toDate: Date().toString(dateFormat: AppDateFormat.ddMMYYYYTransaction), showLoading: false)
             } else {
-                self.presenter?.getWalletHistory(startDate: self.fromToDatePicker.fieldFrom.text!, toDate: self.fromToDatePicker.fieldTo.text!, showLoading: true)
+                self.presenter?.getWalletHistory(startDate: self.fromToDatePicker.fieldFrom.text!, toDate: self.fromToDatePicker.fieldTo.text!, showLoading: false)
             }
             
         })
@@ -111,9 +111,9 @@ class WalletViewController: BaseViewController {
             self.fromToDatePicker.fieldTo.text = date.toString(dateFormat: AppDateFormat.ddMMYYYYTransaction)
             self.presenter?.listWalletHistory.removeAll()
             if self.fromToDatePicker.fieldFrom.text == "" {
-                self.presenter?.getWalletHistory(startDate: "" , toDate: self.fromToDatePicker.fieldTo.text! , showLoading: true)
+                self.presenter?.getWalletHistory(startDate: "" , toDate: self.fromToDatePicker.fieldTo.text! , showLoading: false)
             } else {
-                self.presenter?.getWalletHistory(startDate: self.fromToDatePicker.fieldFrom.text! , toDate: self.fromToDatePicker.fieldTo.text! , showLoading: true)
+                self.presenter?.getWalletHistory(startDate: self.fromToDatePicker.fieldFrom.text! , toDate: self.fromToDatePicker.fieldTo.text! , showLoading: false)
             }
             
         })
@@ -137,6 +137,10 @@ class WalletViewController: BaseViewController {
     @objc func pullToRefresh(){
         presenter?.listWalletHistory.removeAll()
         getWalletHistory(showLoading: false)
+        self.fromToDatePicker.fieldFrom.text = ""
+        self.fromToDatePicker.fieldFrom.placeholder = "dd/mm/yyyy"
+        self.fromToDatePicker.fieldTo.text = ""
+        self.fromToDatePicker.fieldTo.placeholder = "dd/mm/yyyy"
         refreshControl.endRefreshing()
     }
     
@@ -153,7 +157,7 @@ extension WalletViewController: WalletViewProtocol {
     }
     
     // MARK: Get wallet history
-    func getWalletHistory(showLoading: Bool = true) {
+    func getWalletHistory(showLoading: Bool = false) {
         presenter?.getWalletHistory(startDate: "", toDate: "", showLoading: showLoading)
     }
     
